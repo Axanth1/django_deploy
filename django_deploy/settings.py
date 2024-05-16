aacbaddcc09476a1e58558f00d1d2d7428bacac2
Alexandros Xanthopoulos <xanthopoulosa22@gmail.com>
2024-05-16 22:48:07 +0300

    dfsdf

--- django_deploy/settings.py
+++ django_deploy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-hs-bnnll5mh^ponq%39==z)vxm2t91mvfn99cf(_-*d@4x1u8v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
